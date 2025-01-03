aeb815da5a8e46dc00e6d65f457a72faa59b8261
unknown <rhiki.080bct033@tcioe.edu.np>
2025-01-03 19:46:27 +0545

    Changed stuffs

--- arms.gd
+++ arms.gd
@@ -6,16 +6,19 @@ func _ready():
 	anim.play("WEP_Idle")
 
 func _process(delta):
-	if Input.is_action_just_pressed("forward") or Input.is_action_just_pressed("back"):
-		anim.play("WEP_Walk")
-	if Input.is_action_just_released("back") or Input.is_action_just_released("forward"):
+	if Input.is_action_just_pressed("forward") or Input.is_action_just_pressed("back") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right")  :
+		anim.play("WEP_Walk",-1,2)
+	if Input.is_action_just_released("back") or Input.is_action_just_released("forward") or Input.is_action_just_released("left") or Input.is_action_just_released("right")  :
 		anim.play("WEP_Idle")
+	if Input.is_action_pressed("sprint") and Input.is_action_pressed("forward"):
+			anim.play("WEP_Walk",-1,3)
+	
 
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index== MOUSE_BUTTON_LEFT:
-			anim.play("WEP_Fire")
+	if Input.is_action_just_pressed("shoot"):
+		anim.play("WEP_Fire")
+		$Gun.play()
 	if Input.is_action_just_pressed("R"):
 		anim.play("WEP_Reload_01")
 	if Input.is_action_just_pressed("E"):
